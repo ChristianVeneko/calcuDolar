93b7886ab8d91af6f98d70341c41a2c0f2328cce
Christian Mendoza <christianmendozater@gmail.com>
2022-10-16 17:33:30 -0400

    funcion para cargar datos del dolar paralelo

--- main.js
+++ main.js
@@ -6,17 +6,31 @@ const campoPrecio = document.getElementById('tasa');
 const campoDolar = document.getElementById('dolar');
 const campoResultado = document.getElementById('monto');
 
-const url = "https://v6.exchangerate-api.com/v6/572e47397575f899191f4ce2/latest/USD"
-
-const url2= ""
-
-fetch(url)
-.then(res => res.json())
-.then(data => {
+function cargarDatosBCV(){
+    const url = "https://v6.exchangerate-api.com/v6/572e47397575f899191f4ce2/latest/USD"
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
     precio = data
     console.log(precio)
     campoPrecio.innerHTML = precio.conversion_rates.VES
+    return campoPrecio
 })
+}
+
+function cargarDatosParalelo(){
+    fetch("https://exchangemonitor.net/api/divisas?user=CHRISTIAN_121022&token=sM9OWL5mpm6e3akM0Zsv&currency=USD&filter=VES").then((resp) => {
+        if(resp.ok){
+            return resp.json();
+        }
+    }).then((json) => {
+        precio = json.data;
+        console.log(precio);
+        campoPrecio.innerHTML = precio[0].data.rate;
+        return campoPrecio;
+
+    })
+}
 
 function calcular(){
     let price = parseFloat(campoPrecio.textContent);
@@ -27,3 +41,5 @@ function calcular(){
     console.log(result);
 }
 
+
+window.onload = cargarDatosBCV()
