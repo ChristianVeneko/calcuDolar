d564822ae3a2189c25ac39f24be7eaeb647ec360
Christian Mendoza <christianmendozater@gmail.com>
2022-11-16 22:04:54 -0400

    cambio ligeros en el diseño y funcion de dividir agregada

--- main.js
+++ main.js
@@ -6,6 +6,24 @@ const campoPrecio = document.getElementById('tasa');
 const lastUpdate = document.getElementById('lastUpdate')
 const campoDolar = document.getElementById('dolar');
 const campoResultado = document.getElementById('monto');
+const labelMoneda = document.getElementById('moneda')
+const labelMonedaResultante = document.getElementById('monedaResultante')
+
+let usdToBs = true
+
+function cambiarOperacion(){
+    if(usdToBs){
+        usdToBs = false
+        labelMoneda.innerText = "Bs."
+        labelMonedaResultante.innerText = "USD $"
+    }else{
+        usdToBs = true
+        labelMoneda.innerText = "USD $"
+        labelMonedaResultante.innerText = "Bs."
+    }
+    
+}
+
 
 function cargarDatosBCV(){
     const url = "https://v6.exchangerate-api.com/v6/572e47397575f899191f4ce2/latest/USD"
@@ -13,12 +31,11 @@ function cargarDatosBCV(){
     .then(res => res.json())
     .then(data => {
     precio = data
-    console.log(precio)
-    let fecha = new Date(precio.time_last_update_utc)
-    console.log(fecha)
-    campoPrecio.innerHTML = precio.conversion_rates.VES
-    lastUpdate.innerHTML = fecha
-    return campoPrecio
+    console.log(precio);
+    let fecha = new Date(precio.time_last_update_utc);
+    campoPrecio.innerHTML = precio.conversion_rates.VES;
+    lastUpdate.innerHTML = fecha;
+    return campoPrecio;
 })
 }
 
@@ -28,11 +45,10 @@ function cargarDatosParalelo(){
             return resp.json();
         }
     }).then((json) => {
-        
         precio = json.data;
         console.log(precio);
         campoPrecio.innerHTML = precio[0].data.rate;
-        lastUpdate.innerHTML = precio[0].format.date
+        lastUpdate.innerHTML = precio[0].format.date;
         return campoPrecio;
     })
 }
@@ -40,10 +56,15 @@ function cargarDatosParalelo(){
 function calcular(){
     let price = parseFloat(campoPrecio.textContent);
     let mount = campoDolar.valueAsNumber;
-    let result = price * mount;
-    result = result.toFixed(2)
-    campoResultado.valueAsNumber = result;
-    console.log(result);
+    let resultBS = price * mount;
+    let resultUSD = mount / price;
+    if(usdToBs){
+        resultBS = resultBS.toFixed(2);
+        campoResultado.valueAsNumber = resultBS;
+    }else{
+        resultUSD = resultUSD.toFixed(2);
+        campoResultado.valueAsNumber = resultUSD;
+    }
 }
 
 
